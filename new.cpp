db5bc3266866c13df41833efb4e58636adeda08e
VED-PRAKA5H <vihastvideos21@gmail.com>
2024-06-01 12:04:57 +0530

    string

--- new.cpp
+++ new.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "math.h"
+#include <string>
 using namespace std;
 
 int main()
 {
-    int a ;
+    int a;
+    string b;
     cout<< "hello world\nenter a number to find squareroot!";
     cin>>a;
-    cout<<"sqrt of "<<a<<" is "<< sqrt(a);
+    cout<<"what is your name";
+    cin>>b;
+    cout<<b<<"! sqrt of "<<a<<" is "<< sqrt(a);
     return 0;
 }
