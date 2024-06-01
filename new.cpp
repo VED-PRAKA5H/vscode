b5200f026808b312d07f5eb32c30938a9b6f2804
VED-PRAKA5H <vihastvideos21@gmail.com>
2024-06-01 11:44:48 +0530

    .

---
+++ new.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+int main()
+{
+    int a ;
+    cout<< "hello world\nenter a number to find squareroot!";
+    cin>>a;
+    cout<<"sqrt of "<<a<<" is "<< sqrt(a);
+    return 0;
+}
